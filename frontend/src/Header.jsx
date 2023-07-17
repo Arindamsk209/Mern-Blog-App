f9a91d4bdb741faeb8c28061d6760de8d47e053b
Arindam Singh <arindamsingh209@gmail.com>
2023-07-18 00:16:55 +0530

    First-Commit
    First-Date : 17-07-2023

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,47 @@
+import {Link} from "react-router-dom";
+import {useContext, useEffect, useState} from "react";
+import {UserContext} from "./UserContext";
+
+export default function Header() {
+  const {setUserInfo,userInfo} = useContext(UserContext);
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
+
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo"><img src="https://i.pinimg.com/originals/5e/b9/24/5eb924aee830d769dff1ad0997a99d25.gif"></img>
+     </Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logout ({username})</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
---
+++ frontend/src/pages/CreatePost.jsx
@@ -0,0 +1,59 @@
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
+import {useState} from "react";
+import {Navigate} from "react-router-dom";
+import Editor from "../Editor";
+
+export default function CreatePost() {
+  const [title,setTitle] = useState('');
+  const [summary,setSummary] = useState('');
+  const [content,setContent] = useState('');
+  const [files, setFiles] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  async function createNewPost(ev) {
+    const data = new FormData();
+    data.set('title', title);
+    data.set('summary', summary);
+    data.set('content', content);
+    data.set('file', files[0]);
+    ev.preventDefault();
+    const response = await fetch('http://localhost:4000/post', {
+      method: 'POST',
+      body: data,
+      credentials: 'include',
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
+  return (
+    <form onSubmit={createNewPost}>
+      <input type="title"
+             placeholder={'Title'}
+             value={title}
+             onChange={ev => setTitle(ev.target.value)} />
+      <input type="summary"
+             placeholder={'Summary'}
+             value={summary}
+             onChange={ev => setSummary(ev.target.value)} />
+      <input type="file"
+             onChange={ev => setFiles(ev.target.files)} />
+      <Editor value={content} onChange={setContent} />
+      <button style={{marginTop:'5px'}}>Create post</button>
+    </form>
+  );
+}
+// {
+//   "dependencies": {
+//     "bcryptjs": "^2.4.3",
+//     "cookies": "^0.8.0",
+//     "cors": "^2.8.5",
+//     "express": "^4.18.2",
+//     "multer": "^1.4.5-lts.1",
+//     "parser": "^0.1.4"
+//   }
+// }

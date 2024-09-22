318f36ed8d81f20396366db5d7668a5601781cdc
Arindamsk209 <91045455+Arindamsk209@users.noreply.github.com>
2024-09-22 14:26:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:true,origin:'http://localhost:5173'}));
+app.use(cors({credentials:true,origin:'https://mern-blog-app-frontend-two.vercel.app'}));
 
 app.use(express.json());
 app.use(cookieParser());

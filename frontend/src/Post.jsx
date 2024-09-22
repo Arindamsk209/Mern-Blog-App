31a98a46d4277a02d764490c951a6ace6aba453f
Arindamsk209 <91045455+Arindamsk209@users.noreply.github.com>
2024-09-22 15:47:41 +0530

    Update Post.jsx

--- frontend/src/Post.jsx
+++ frontend/src/Post.jsx
@@ -8,7 +8,10 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
    <div className="post">
      <div className="image">
       <Link to={`/post/${_id}`}>
-        <img src={`https://mern-blog-app-backend-tgj1.onrender.com/${cover.replace(/\\/g, '/')}`} alt="Post Cover" />
+       <img 
+        src={`https://mern-blog-app-backend-tgj1.onrender.com/${cover.startsWith('/') ? cover.slice(1) : cover}`} 
+        alt="Post Cover" 
+      />
       </Link>
     </div>
       <div className="texts">
